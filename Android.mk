1cde93e1c2ff788e22c26d1cedb7cb69858f5d55
Alex Cruz <mazda@UnicornBox.(none)>
2014-03-04 18:48:43 -0600

    Remove app drawer icon / make priveleged app
    Conflicts:
    	Android.mk
    	app/src/main/AndroidManifest.xml

--- Android.mk
+++ Android.mk
@@ -1,3 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/app/libs/Android.mk $(LOCAL_PATH)/app/src/main/Android.mk
+
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := KernelTweaker
